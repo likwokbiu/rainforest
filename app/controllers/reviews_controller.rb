5c7443f3ee2c67ee477c8edd66e818fbc7bff310
likwokbiu <li_bill@ymail.com>
2018-02-28 14:48:26 -0500

    Add comment creation

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,17 @@
 class ReviewsController < ApplicationController
 
   def create
+    @review = Review.new
+
+    @review.comment = params[:review][:comment]
+    @review.product_id = params[:product_id]
+
+    if @review.save
+      redirect_to "/products/#{params[:product_id]}"
+    else
+      flash[:alert] = "Review cannot be empty!"
+      redirect_to "/products/#{params[:product_id]}"
+    end
 
   end
 
