d9639aa263401af287a2b10b326d031b7b642dcd
likwokbiu <li_bill@ymail.com>
2018-02-28 12:39:16 -0500

    Add product method for creation

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -14,6 +14,17 @@ class ProductsController < ApplicationController
   end
 
   def create
+    @product = Product.new
+
+    @product.name = params[:product][:name]
+    @product.description = params[:product][:description]
+    @product.price_in_cents = params[:product][:price_in_cents]
+
+    if @product.save
+      redirect_to products_path
+    else
+      render :new
+    end
 
   end
 
