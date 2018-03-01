7f026b44ddd4c018e1b52496e038bae6b58c0902
dmitrysbn <dmitry.sbn@gmail.com>
2018-03-01 15:20:46 -0500

    Add sessions model, routes, method, and log in/log out/sign up links that work as intended

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'products#index'
@@ -8,5 +14,7 @@ Rails.application.routes.draw do
   end
 
   resource :users, only: %i(new create)
+
+  resource :sessions, only: %i(new create destroy)
 
 end
